eacbfe08a4122bdde0d555c342347e835cfccce5
maxdark1 <max_lara3005@hotmail.com>
2022-06-02 23:15:59 -0500

    Se agregan las imagenes a webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
         extensions: [
             '.js',
             '.jsx'
-        ]
+        ],
+        alias : {
+            '@components' :path.resolve(__dirname, 'src/components/'),
+            '@containers' :path.resolve(__dirname, 'src/containers/'),
+            '@pages' :path.resolve(__dirname, 'src/pages/'),
+            '@routes' :path.resolve(__dirname, 'src/routes/'),
+            '@styles' :path.resolve(__dirname, 'src/styles/'),
+            '@logos' :path.resolve(__dirname, 'src/logos/'),
+            '@icons' :path.resolve(__dirname, 'src/icons/'),
+        }
     },
     module: {
         rules: [
@@ -38,8 +47,11 @@ module.exports = {
                     "css-loader",
                     "sass-loader",
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset'
             }
-
         ]
     },
     plugins: [
